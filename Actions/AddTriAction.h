04f0292f20fb031a25d688471d3daa8a3514ae02
elshafa73 <karimelshafai05@gmail.com>
2025-05-01 21:25:59 +0300

    Update AddTriAction.h

--- Actions/AddTriAction.h
+++ Actions/AddTriAction.h
@@ -1,1 +1,12 @@
 #pragma once
+#include"Action.h"
+class AddTriAction : public Action
+{
+private:
+	Point Vertex1, Vertex2, Vertex; 
+	GfxInfo TriGfxInfo;
+public:
+	AddTriAction(ApplicationManager* pApp);
+	virtual void ReadActionParameters();
+	virtual void Execute();
+};
